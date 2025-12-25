80b77be3c742b6e3d1890d72d0efe78570adc59e
Zuckmayer91 <bz_91.k@hotmail.com>
2025-12-25 00:47:49 -0500

    order-db-add

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+    
 class Post(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    # Cambiamos auto_created por auto_now_add
+    order = models.IntegerField(default=0)  # Nuevo campo para el orden
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['order']  # Esto hace que Django ordene los posts por defecto
