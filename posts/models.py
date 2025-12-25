1f56d4a4b218dc4a914720d1e35f1c3a4ae9ab19
Zuckmayer91 <bz_91.k@hotmail.com>
2025-12-24 23:59:37 -0500

    tabladb

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    # Cambiamos auto_created por auto_now_add
+    created_at = models.DateTimeField(auto_now_add=True)
