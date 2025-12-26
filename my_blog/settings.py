9dafdf6486215d00a7be9475f71956d7933f7961
Zuckmayer91 <bz_91.k@hotmail.com>
2025-12-26 17:24:50 -0500

    new_db

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
     'rest_framework',
     'drf_yasg',
     'posts'
@@ -119,3 +120,5 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = './static'
+
+AUTH_USER_MODEL= 'users.User'
