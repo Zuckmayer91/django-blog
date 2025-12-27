e7a3f2131b3eb6d33e61ae5e5943505b9f20364f
Zuckmayer91 <bz_91.k@hotmail.com>
2025-12-27 08:12:05 -0500

    usuarios-configurados

--- users/admin.py
+++ users/admin.py
@@ -4,15 +4,11 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
+    # Esto hará que en la lista principal veas primero el email
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff')
+    
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('Información Personal', {
-            'fields': ('first_name', 'last_name', 'email', 'web_site', 'twitter', 'facebook', 'instagram')
-        }),
-        ('Permisos', {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
-        }),
-        ('Fechas Importantes', {
-            'fields': ('last_login', 'date_joined')
-        }),
+        ('Información Personal', {'fields': ('first_name', 'last_name', 'email', 'web_site', 'twitter', 'facebook', 'instagram')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
     )
--- users/models.py
+++ users/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    # Hacemos que el email sea obligatorio y único
+    email = models.EmailField(unique=True) 
+    
     web_site = models.CharField(max_length=255, blank=True)
     twitter = models.CharField(max_length=255, blank=True)
     facebook = models.CharField(max_length=255, blank=True)
     instagram = models.CharField(max_length=255, blank=True)
+
+    # Establecemos el email como el identificador principal
+    USERNAME_FIELD = 'email'
+    # Campos obligatorios además del password y email al crear superuser
+    REQUIRED_FIELDS = ['username']
    
