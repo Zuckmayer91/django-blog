a08f25b12a85b0d6c841d7b7557fca60ae54d00a
Zuckmayer91 <bz_91.k@hotmail.com>
2025-12-27 19:22:52 -0500

    postgret-exitoso

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
 import datetime 
 from pathlib import Path
 
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt',
     'users',
     'rest_framework',
     'drf_yasg',
@@ -135,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = './static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Mejor práctica para Docker
 
-AUTH_USER_MODEL= 'users.User'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
